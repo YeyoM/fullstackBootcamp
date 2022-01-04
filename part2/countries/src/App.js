2f5c2ef7690d2cfbe8ca335b9f3cc85a58d76d3b
YeyoM <yeyomoreno2003@hotmail.com>
2022-01-04 14:18:22 -0600

    countries almost finished

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -6,19 +6,31 @@ function App() {
 
   const [countries, setCountries] = useState([]);
   const [newFilter, setNewFilter] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get('https://restcountries.com/v3.1/all')
     .then(response => {
+      setIsLoading(false);
       const {data} = response;
       setCountries(data);
     })
     .catch(err => console.error(err));
-  }, [newFilter]);
-  
+  }, []);
+
   const handleFilterInput = (event) => {
     setNewFilter(event.target.value);
   }
+
+  let matchingCountriesNumber = 0
+
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].name.common.toLowerCase().includes(newFilter.toLocaleLowerCase()) === true){
+      matchingCountriesNumber++;
+    }
+  }
+
 
   return (
     <div className="App">
@@ -26,17 +38,51 @@ function App() {
         Find countries: <input type="text" onChange={handleFilterInput}/>
       </div>
       <h2>Countries</h2>
-        {
-          countries
+      {
+        isLoading 
+        ? 'Loading...' 
+        : ''
+      }
+      { matchingCountriesNumber >= 10
+        ? 'Too many matches, please be more specific' 
+        : countries
           .filter((country) => {
             if (country.name.common.toLowerCase().includes(newFilter.toLocaleLowerCase()) === true){
               return country
             }
           })
           .map((country) => {
-            return <p key={country.population + country.area}>{country.name.common}</p>
+            return (
+              <h3 key={country.population + country.area}>{country.name.common}</h3>
+            )
+          }) 
+      }
+      { matchingCountriesNumber === 1 
+        ? countries
+          .filter((country) => {
+            if (country.name.common.toLowerCase().includes(newFilter.toLocaleLowerCase()) === true){
+              return country
+            }
           })
-        }
+          .map((country) => {
+            return (
+              <div key={country.population + country.area}>
+                <p>Capital:    {country.capital}</p>
+                <p>Population: {country.population}</p>
+                <h4>Languages</h4>
+                  {
+                    Object.values(country.languages).map((lang) => {
+                      return (
+                        <p>{lang}</p>
+                      )
+                    })
+                  }
+                <img src={country.flags.png} alt="Flag" />
+              </div>
+            )
+          })
+        : '' 
+      }
     </div>
   );
 }
