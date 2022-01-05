df05aec7d0a9d4af39ccc77dd4fb27861b5f518e
YeyoM <yeyomoreno2003@hotmail.com>
2022-01-04 19:48:58 -0600

    countriesApp almost finished

--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -1,29 +1,62 @@
 import {useState} from 'react'
+import OneCountry from './OneCountry'
 
 const CountryList = ({countries, newFilter}) => {
 
-    const [showOneCountry, setShowOneCountry] = useState('');
+    const [showOneCountry, setShowOneCountry] = useState(false);
+    const [countryToShow, setCountryToShow] = useState('');
 
     // tenemos que evaluar si el estado de arriba es true, y en caso de que lo sea,ver el pais que nos marca, lo podemos recuperar del boton supongo
+
+    const handleOneCountry = (event) => {
+        const countryBtn = event.target.innerHTML.slice(5);
+        setShowOneCountry(true);
+        setCountryToShow(countryBtn);
+    }
 
     return (
         <div>
             {
-            countries 
-            .filter((country) => {
-              if (country.name.common.toLowerCase().includes(newFilter.toLocaleLowerCase()) === true){
-                return country
-              }
-            })
-            .map((country) => {
-              return (
-                <div>
-                    <h3 key={country.population + country.area}>{country.name.common}</h3>
-                    <button>show</button>
-                </div>
-              )
-            }) 
-        }
+                showOneCountry 
+                ? countries
+                .filter((country) => {
+                  if (country.name.common.toLowerCase() === countryToShow.toLowerCase()){
+                    return country
+                  }
+                })
+                .map((country) => {
+                  return (
+                    <div key={country.population + country.area}>
+                      <h3>{country.name.common}</h3>
+                      <p>Capital:    {country.capital}</p>
+                      <p>Population: {country.population}</p>
+                      <h4>Languages</h4>
+                        {
+                          Object.values(country.languages).map((lang) => {
+                            return (
+                                <p>{lang}</p>
+                            )
+                          })
+                        }
+                      <img src={country.flags.png} alt="Flag" />
+                    </div>
+                  )
+                })
+                : countries 
+                .filter((country) => {
+                  if (country.name.common.toLowerCase().includes(newFilter.toLocaleLowerCase()) === true){
+                    return country
+                  }
+                })
+                .map((country) => {
+                  return (
+                    <div>
+                        <h3 key={country.population + country.area}>{country.name.common}</h3>
+                        <button onClick={handleOneCountry} key={country.name.common}>show {country.name.common}</button>
+                    </div>
+                  )
+                }) 
+            }
         </div>
     )
 }
--- part2/countries/src/components/OneCountry.js
+++ part2/countries/src/components/OneCountry.js
@@ -1,10 +1,45 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 const OneCountry = ({countries, newFilter}) => {
+
+  const [weather, setWeather] = useState([]);
+  const [weatherLoad, setWeatherLoad] = useState(true);
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  let country
+
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].name.common.toLowerCase().includes(newFilter.toLowerCase()) === true) {
+      country = countries[i].name.common
+    }
+  }
+
+  console.log(weather)
+
+  useEffect(() => {
+    setWeatherLoad(true)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${apiKey}`)
+    .then(response => {
+      const {data} = response;
+      setWeather(data);
+      setWeatherLoad(false)
+      console.log('listo');
+    })
+    .catch(err => console.error(err));
+  }, [country, apiKey]);
+
+  console.log(weather)
+
     return (
         <div>
             {
+              weatherLoad
+              ? 'Cargando...'
+              :
                 countries
                 .filter((country) => {
-                  if (country.name.common.toLowerCase().includes(newFilter.toLocaleLowerCase()) === true){
+                  if (country.name.common.toLowerCase().includes(newFilter.toLowerCase()) === true){
                     return country
                   }
                 })
@@ -22,6 +57,10 @@ const OneCountry = ({countries, newFilter}) => {
                           })
                         }
                       <img src={country.flags.png} alt="Flag" />
+                      <h4>Weather in {country.capital}</h4>
+                      <p><strong>Weather-like:</strong> {weather.weather[0].description}</p>
+                      <p><strong>Temperature: </strong> {Math.round(weather.main.temp - 273)}°C</p>
+                      <p><strong>Wind: </strong> {weather.wind.speed}mph </p>
                     </div>
                   )
                 })
